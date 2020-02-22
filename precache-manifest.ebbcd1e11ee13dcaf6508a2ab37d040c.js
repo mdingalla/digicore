acba867d555bc45570ab63054da9e5c46cdd2ec2
mdingalla <mdingalla@gmail.com>
2020-02-22 12:06:59 +0800

    Updates

---
+++ precache-manifest.ebbcd1e11ee13dcaf6508a2ab37d040c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371d07af223adfd1e18947f20aa91f17",
+    "url": "/digicore/index.html"
+  },
+  {
+    "revision": "d6e254e070adc0e8caec",
+    "url": "/digicore/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/digicore/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/digicore/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e254e070adc0e8caec",
+    "url": "/digicore/static/js/main.0c19d48f.chunk.js"
+  },
+  {
+    "revision": "72b799c8143209401bd8",
+    "url": "/digicore/static/js/runtime-main.22a7ad0a.js"
+  }
+]);
