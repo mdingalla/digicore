a9ec7c17aab8323ee375b81474ee71e858ef0965
mdingalla <mdingalla@gmail.com>
2020-02-22 11:55:06 +0800

    first commit

---
+++ src/component/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class MainApp extends React.Component<any,any>{
+    constructor(props:any){
+        super(props);
+    }
+
+
+    render(){
+        return <div>
+            Main App
+        </div>
+    }
+}
+
+export default MainApp;
